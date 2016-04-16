de752176328f647833eaa6da3fe841391888c08f
Beer van der Drift <beerdrift@gmail.com>
2016-04-16 19:42:05 +0200

    Made the site look like the previous version.

---
+++ client/main.js
@@ -0,0 +1,8 @@
+redirect = function(){ 
+  window.location = "http://bit.ly/tokomooi"; 
+};
+
+//Music won't play on mobile phones, so normal redirect.
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  setTimeout(redirect, 5000);
+}
