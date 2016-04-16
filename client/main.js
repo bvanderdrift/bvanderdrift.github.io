1d524795017f070482a4a93d016d4eb0ef43e420
Beer van der Drift <beerdrift@gmail.com>
2016-04-16 22:08:50 +0200

    Added device tracking.

--- client/main.js
+++ client/main.js
@@ -5,6 +5,10 @@ redirect = function(){
 };
 
 //Music won't play on mobile phones, so normal redirect.
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+if(Meteor.Device.isPhone() || Meteor.Device.isTablet()) {
   setTimeout(redirect, 5000);
 }
+
+analytics.ready(function(){
+	analytics.track("Visit by " + Meteor.Device._type);
+});
